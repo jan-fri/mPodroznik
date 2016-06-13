adc689c110b268f27301904cdef693fb7153a935
janfri <janfri@wp.pl>
2016-06-13 19:59:43 +0200

    cd2

---
+++ mPodroznik.Domain/DAL/BusInitializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using mPodroznik.Domain.Entities;
+
+namespace mPodroznik.Domain.DAL
+{
+    public class BusInitializer : DropCreateDatabaseIfModelChanges<BusContex>
+    {
+        protected override void Seed(BusContex context)
+        {
+            // base.Seed(context);
+            var buses = new List<Bus>
+            {
+               new Bus {BusNumber = "1" },
+               new Bus {BusNumber= "2" }
+            };
+
+            buses.ForEach(b => context.Buses.Add(b));
+            context.SaveChanges();
+
+            var busStops = new List<BusStop>
+            {
+                new BusStop {Name = "Cygański Las", BusID = 1},
+                new BusStop {Name = "Bystrzańska Olszówka", BusID = 1},
+                new BusStop {Name = "Długa Zajezdnia MZK", BusID = 1},
+                new BusStop {Name = "Armii Krajowej Szpital", BusID = 1},
+                new BusStop {Name = "Partyzantów Folwark", BusID = 1 },
+                new BusStop {Name = "Partyzantów Apena", BusID = 1 },
+                new BusStop {Name = "Plac Mickiewicza", BusID = 1 },
+                new BusStop {Name = "Plac Żwirki i Wigury", BusID = 1},
+                new BusStop {Name = "Hotel Prezydent", BusID = 1},
+                new BusStop {Name = "3 Maja Dworzec", BusID = 1 },
+                new BusStop {Name = "Piastowska Lubertowicza", BusID = 1 },
+                new BusStop {Name = "Piastowska Wita Stwosza", BusID = 1},
+                new BusStop {Name = "Piastowska Sobieskiego", BusID = 1 },
+                new BusStop {Name = "Cieszyńska Hulanka", BusID = 1},
+                new BusStop {Name = "Szarotki Cieszyńska", BusID = 1 },
+                new BusStop {Name = "Szarotki SAM", BusID = 1}
+            };
+            busStops.ForEach(b => context.BusStops.Add(b));
+            //context.SaveChanges();
+            base.Seed(context);
+
+        }
+    }
+}
---
+++ mPodroznik.Domain/Entities/BusContex.cs
@@ -0,0 +1,21 @@
+﻿using mPodroznik.Domain.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mPodroznik.Domain.Entities
+{
+    public class BusContex : DbContext
+    {
+        public BusContex() : base("BusContext")
+        {
+            Database.SetInitializer<BusContex>(new BusInitializer());
+        }
+
+        public DbSet<Bus> Buses { get; set; }
+        public DbSet<BusStop> BusStops { get; set; }
+    }
+}
